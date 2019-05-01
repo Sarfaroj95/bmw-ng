630466656a06cea4aa7e11ea548a0276f85c0256
Sarfaroj95 <sarfaroj95@gmail.com>
2019-05-01 15:44:53 +0530

    add rental list component, add rental details component, add rental service and rental model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
+import { RentalComponent } from './rental/rental.component';
+
+import { RentalModule } from './rental/rental.module'; 
+
+const routes : Routes = [
+        {path: '', redirectTo: './rentals', pathMatch: 'full'}
+]
+
+
 @NgModule({
-  declarations: [
+  declarations: [  
     AppComponent,
     HeaderComponent,
     AuthComponent,
     LoginComponent,
     RegisterComponent
+
   ],
+
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/rental/rental-list-item/rental-list-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'bmw-rental-list-item',
+  templateUrl: './rental-list-item.component.html',
+  styleUrls: ['./rental-list-item.component.scss']
+})
+export class RentalListItemComponent implements OnInit {
+
+	@Input() rental: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+ 
